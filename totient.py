e0b1448cafb4e8e4d4f6e4beb0856ce1463c19f4
Restyled.io <commits@restyled.io>
2020-10-22 13:20:44 +0000

    Restyled by autopep8

--- totient.py
+++ totient.py
@@ -1,18 +1,19 @@
-def tot(n) :
+def tot(n):
     result = n
     p = 2
-    while p <= n**0.5 :
+    while p <= n**0.5:
 
-        if n % p == 0 :
+        if n % p == 0:
             result = result * (1.0 - (1.0 / float(p)))
-            while n % p == 0 :
+            while n % p == 0:
                 n = n // p
         p = p + 1
-        
-    if n > 1 :
+
+    if n > 1:
         result = result * (1.0 - (1.0 / float(n)))
 
     return int(result)
 
-for n in range(1, 11) :
+
+for n in range(1, 11):
     print("tot(", n, ") = ", phi(n))
