7d4bd20ef82fcc631afe27f7fca8c26abc8dc1c0
Restyled.io <commits@restyled.io>
2020-10-22 13:19:51 +0000

    Restyled by autopep8

--- ExtGcd.py
+++ ExtGcd.py
@@ -1,22 +1,23 @@
-def xgcd(a,b):
-    x = 0;
-    y = 1;
-    t1 =  1;
-    t2 = 0;
+def xgcd(a, b):
+    x = 0
+    y = 1
+    t1 = 1
+    t2 = 0
 
     while b != 0:
 
-        temp = b;
-        quotient = int(a // b); # getting quotient
-        b = a % b; # getting remainder
-        a = temp;
-        temp = x;
-        x = t1 -quotient*x;
-        t1 = temp;
-        temp = y;
-        y = t2-quotient*y;
-        t2 = temp;
-    print(a)   
-    return [t1,t2];
+        temp = b
+        quotient = int(a // b)  # getting quotient
+        b = a % b  # getting remainder
+        a = temp
+        temp = x
+        x = t1 - quotient*x
+        t1 = temp
+        temp = y
+        y = t2-quotient*y
+        t2 = temp
+    print(a)
+    return [t1, t2]
 
-xgcd(30,20)
+
+xgcd(30, 20)
