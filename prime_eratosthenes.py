59d309b022bb30b729f9af09723ace15d4c55b9f
Restyled.io <commits@restyled.io>
2020-10-22 13:18:54 +0000

    Restyled by autopep8

--- prime_eratosthenes.py
+++ prime_eratosthenes.py
@@ -2,8 +2,9 @@ def prime_eratosthenes(n):
     prime_list = []
     for i in range(2, n+1):
         if i not in prime_list:
-            print (i)
+            print(i)
             for j in range(i*i, n+1, i):
                 prime_list.append(j)
 
-print(prime_eratosthenes(20));
+
+print(prime_eratosthenes(20))
